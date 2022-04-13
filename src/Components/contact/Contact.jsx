6d9e9358d36771146582793486ebb316cf513340
brysonwaisi <brysonnyamwange@gmail.com>
2022-04-14 01:46:12 +0300

    Finished Project

--- src/Components/contact/Contact.jsx
+++ src/Components/contact/Contact.jsx
@@ -1,9 +1,58 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import './Contact.css'
+import { MdEmail } from 'react-icons/md'
+import { FaFacebookMessenger } from 'react-icons/fa'
+import { FaWhatsapp } from 'react-icons/fa'
+import emailjs from '@emailjs/browser'
 
 const Contact = () => {
+
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_v18xzsq', 'template_6sifady', form.current, 'qCTIl-A5butAP0y4v')
+      
+      e.target.reset();
+  };
+
   return (
-    <section id='contact'>Contact</section>
+    <section id='contact'>
+      <h5>Get in Touch </h5>
+      <h2>Contact Me</h2>
+
+      <div className="container contact__container">
+        <div className="contact__options">
+          <article className="contact__option">
+            <MdEmail className='contact__option-icon'/>
+            <h4>Email</h4>
+            <h5>brysonnyamwange@gmail.com</h5>
+            <a href="mailto:brysonnyamwange@gmail.com" target='_blank'>Send a Message</a>
+          </article>
+
+          <article className="contact__option">
+            <FaFacebookMessenger className='contact__option-icon'/>
+            <h4>Messenger</h4>
+            <h5>Bryson Nyamwange</h5>
+            <a href="https://m.me/bryson.nyamwange" target='_blank'>Send a Message</a>
+          </article>
+
+          <article className="contact__option">
+            <FaWhatsapp className='contact__option-icon'/>
+            <h4>WhatsApp</h4>
+            <a href="https://wa.me/254713015955" target='_blank'>Send a Message</a>
+          </article>
+        </div>
+        {/* END OF CONTACT OPTIONS */}
+        <form ref={form} onSubmit={sendEmail}>
+          <input type="text" name='name' placeholder='Your Full Name' required />
+          <input type="email" name='email' placeholder='Your Email' required/>
+          <textarea name="message" placeholder='Your Message' rows="7" required></textarea>
+          <button type='submit' className='btn btn-primary'>Send Message</button>
+        </form>
+      </div>
+    </section>
   )
 }
 
