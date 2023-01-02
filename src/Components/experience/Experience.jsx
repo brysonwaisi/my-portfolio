15f546382c5dfb8d4818d6de79a227d7ccc1ff99
Bryson Nyamwange <brysonnyamwange@gmail.com>
2023-01-02 17:29:40 +0300

    Update Experience.jsx

--- src/Components/experience/Experience.jsx
+++ src/Components/experience/Experience.jsx
@@ -80,7 +80,7 @@ const Experience = () => {
               <BsPatchCheckFill className='experience__details-icon'/>
               <div>
                 <h4>SQL</h4>
-                <small className="text-light">Basic</small>
+                <small className="text-light">Intermediate</small>
               </div>
             </article>
             <article className="experience__details">
