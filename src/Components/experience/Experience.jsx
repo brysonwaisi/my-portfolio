91b5d0fc654fe18420592d9da16eb35256b5c739
Bryson Nyamwange <brysonnyamwange@gmail.com>
2023-03-22 01:15:55 +0300

    Changes

--- src/Components/experience/Experience.jsx
+++ src/Components/experience/Experience.jsx
@@ -83,13 +83,13 @@ const Experience = () => {
                 {/* <small className="text-light">Intermediate</small> */}
               </div>
             </article>
-            <article className="experience__details">
+            {/* <article className="experience__details">
               <BsPatchCheckFill className='experience__details-icon'/>
               <div>
                 <h4>REST API</h4>
-                {/* <small className="text-light">Intermediate</small> */}
+                <small className="text-light">Intermediate</small>
               </div>
-            </article>
+            </article> */}
             <article className="experience__details">
               <BsPatchCheckFill className='experience__details-icon'/>
               <div>
